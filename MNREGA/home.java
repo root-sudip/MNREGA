7d75b68851254dfd8a8e70c7ff54e5b7355ffd9b
root-sudip <touch.das@gmail.com>
2016-03-05 17:13:21 +0530

    Under dev

---
+++ MNREGA/home.java
@@ -0,0 +1,157 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+
+class JError{
+
+	public void login_error(){
+		JPanel panel = new JPanel();
+		JLabel error_label= new JLabel("Enter correct email id or password");
+		JFrame mm = new JFrame();
+
+		mm.setLayout(null);
+		error_label.setBounds(50,40,400,30);
+		mm.setLocationRelativeTo(null);
+		mm.add(error_label);
+		mm.setVisible(true);
+		mm.setResizable(false);	
+		mm.setSize(350,100);
+	}	
+}
+
+class JHome{
+	private static JHome home_obj = new JHome();
+	private JHome(){}
+	public static JHome getInstance(){
+			return home_obj;
+	}
+	
+	Runnable runner = new Runnable(){
+			public void run(){
+				JFrame frame = new JFrame("The Mahatma Gandhi National Rural Employment Guarantee Act");
+				
+				JLabel login_emailid = new JLabel("Email Id");
+				JLabel login_password = new JLabel("Passwotrd");
+				JTextField login_text_emailid = new JTextField("Email Id");
+				JPasswordField login_text_password = new JPasswordField("Password");
+				JButton login_btn = new JButton("Login");
+				JButton back = new JButton("Back");
+				JButton ctrate_project = new JButton("Create Project");
+
+
+				String username="admin";
+				String password="pass";
+
+				frame.setLayout(null);
+				
+				
+				login_emailid.setBounds(300,150,100,100);
+				frame.add(login_emailid);
+
+				login_password.setBounds(300,250,100,100);
+				frame.add(login_password);
+				//text area
+				login_text_emailid.setBounds(300,220,300,50);
+				frame.add(login_text_emailid);
+
+				login_text_password.setBounds(300,320,300,50);
+				frame.add(login_text_password);
+
+				//button
+				login_btn.setBounds(380,380,120,50);
+				frame.add(login_btn);
+
+				back.setBounds(380,30,120,20);
+				frame.add(back);
+				back.setVisible(false);
+
+				ctrate_project.setBounds(700,30,140,20);
+				frame.add(ctrate_project);
+				ctrate_project.setVisible(false);
+
+				
+				//action listener
+				MouseAdapter login_listener = new MouseAdapter(){
+					public void mouseClicked(MouseEvent e){
+						login_text_emailid.setText("");
+					}
+					
+				};
+
+				MouseAdapter password_listener = new MouseAdapter(){
+					public void mouseClicked(MouseEvent e){
+						login_text_password.setText("");
+					}
+					
+				};
+
+				ActionListener listener = new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						if(username.equals(login_text_emailid.getText()) && password.equals(login_text_password.getText())){
+							System.out.println("success!!");
+							login_text_emailid.setVisible(false);
+							login_text_password.setVisible(false);
+							login_emailid.setVisible(false);
+							login_password.setVisible(false);
+							login_btn.setVisible(false);
+							back.setVisible(true);
+							ctrate_project.setVisible(true);
+						}
+						else{
+							System.out.println("Errot:");
+							JError  lerr = new JError();
+							lerr.login_error();
+						}
+					}
+				};
+
+				ActionListener listener_back = new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						login_text_emailid.setVisible(true);
+						login_text_password.setVisible(true);
+						login_emailid.setVisible(true);
+						login_password.setVisible(true);
+						login_btn.setVisible(true);
+						back.setVisible(false);
+						ctrate_project.setVisible(false);
+					}
+				};
+
+				ActionListener listener_create_p = new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						back.setVisible(false);
+						ctrate_project.setVisible(false);
+					}
+				};
+
+
+
+				login_text_emailid.addMouseListener(login_listener);
+				login_text_password.addMouseListener(password_listener);
+				login_btn.addActionListener(listener);
+				back.addActionListener(listener_back);
+				ctrate_project.addActionListener(listener_create_p);
+				
+				
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				//frame.setLayout(new GridLayout(6,7));
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
+				frame.setResizable(false);	
+				frame.setSize(900,600);		
+				//frame.setUndecorated(true);
+				
+			}
+		};
+}
+
+public class home{
+	public static void main(String[] args){
+		
+		JHome obj = JHome.getInstance();
+		obj.runner.run();
+		
+	}
+}
