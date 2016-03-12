d0831b223c1d578fecc6fdb37dcbb0ca004be14f
root-sudip <touch.das@gmail.com>
2016-03-13 00:29:03 +0530

    MNREGA

--- MNREGA/command.sql
+++ MNREGA/command.sql
@@ -2,6 +2,11 @@ create database MNREGA;
 create table project_by_BDO(
 	SLN int primary key not null auto_increment,
 	Project_Name varchar(255),
-	Signed_BDO_Name varchar(255)
+	Signed_BDO_Name varchar(255),
+	Date_Of_Issue date,
+	Panchayat_Name varchar(255),
+	Recived_Panchayat_member_name varchar(255),
+	Money double,
+	Duration varchar(255)
 );
 
