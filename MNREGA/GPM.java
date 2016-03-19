9560f25a77470929e6f5660f1bb6279a3669827e
root-sudip <touch.das@gmail.com>
2016-03-19 12:33:36 +0530

    MNREGA

---
+++ MNREGA/GPM.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+import java.sql.*;
+
+public class GPM{
+	public static void main(String[] args){
+		Runnable run = new Runnable(){
+			public void run(){
+
+			}
+		};
+		run.run();
+	}
+}
