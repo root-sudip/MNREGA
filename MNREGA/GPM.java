2dccddcd83dd1604a69af25d2300a6ee18ebf6d2
root-sudip <touch.das@gmail.com>
2016-03-19 12:54:26 +0530

    MNREGA

--- MNREGA/GPM.java
+++ MNREGA/GPM.java
@@ -13,6 +13,30 @@ public class GPM{
 				JLabel login_emailid = new JLabel("Email Id");
 				JLabel login_password = new JLabel("Password");
 				JButton login_btn = new JButton("Login");
+
+				String username="admin";
+				String password="pass";
+
+
+				frame.setLayout(null);
+
+
+
+				login_emailid.setBounds(300,150,100,100);
+				frame.add(login_emailid);
+
+				login_password.setBounds(300,250,100,100);
+				frame.add(login_password);
+				//text area
+				login_text_emailid.setBounds(300,220,300,50);
+				frame.add(login_text_emailid);
+
+				login_text_password.setBounds(300,320,300,50);
+				frame.add(login_text_password);
+
+				//button
+				login_btn.setBounds(380,380,120,50);
+				frame.add(login_btn);
 			}
 		};
 		run.run();
