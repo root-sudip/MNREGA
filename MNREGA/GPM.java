a7dfcdd6f590d0f170e352f38b47671a2ed8741a
root-sudip <touch.das@gmail.com>
2016-03-19 12:53:47 +0530

    MNREGA

--- MNREGA/GPM.java
+++ MNREGA/GPM.java
@@ -9,7 +9,10 @@ public class GPM{
 	public static void main(String[] args){
 		Runnable run = new Runnable(){
 			public void run(){
-
+				JFrame frame = new JFrame("The Mahatma Gandhi National Rural Employment Guarantee Act");
+				JLabel login_emailid = new JLabel("Email Id");
+				JLabel login_password = new JLabel("Password");
+				JButton login_btn = new JButton("Login");
 			}
 		};
 		run.run();
